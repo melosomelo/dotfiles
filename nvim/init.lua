b82bde58ffa47eb7ef62739f7ddcbffc12321f45
melosomelo <mateusmelornrn@gmail.com>
2022-05-09 12:52:59 -0300

    synchronization commit 09/05/22 - 12:52:59

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,3 @@
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
--- Configuration for Dracula Pro
-vim.cmd[[
-  packadd! dracula_pro
-  syntax enable
-  let g:dracula_colorterm = 0
-  let g:dracula_bold = 1
-  let g:dracula_italic = 1
-  colorscheme dracula_pro
-]]
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -5,6 +5,15 @@ vim.cmd([[
     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
   augroup end
 ]])
+-- Configuration for Dracula Pro
+vim.cmd[[
+  packadd! dracula_pro
+  syntax enable
+  let g:dracula_colorterm = 0
+  let g:dracula_bold = 1
+  let g:dracula_italic = 1
+  colorscheme dracula_pro
+]]
 return require('packer').startup(function()
   -- Lualine
   use {
