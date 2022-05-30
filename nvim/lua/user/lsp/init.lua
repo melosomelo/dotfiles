a162973342287f7963c91a3c280e75156079cef2
melosomelo <mateusmelornrn@gmail.com>
2022-05-30 09:33:53 -0300

    synchronization commit 30/05/22 - 09:33:53

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -3,9 +3,13 @@ local lspconfig = require "lspconfig"
 
 lsp_installer.setup {} -- add more configuration later
 
+-- for each installed server, check if it has custom options
+-- and merge them with default opts
 local installed_servers = lsp_installer.get_installed_servers()
 for _, server in pairs(installed_servers) do
-  local opts = {}
+  local opts = {
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  }
   local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings" .. server.name)
   if has_custom_opts then
     opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
