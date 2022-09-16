8c2df7b9c6f180719d21a27db9f2340132615c57
melosomelo <mateusmelornrn@gmail.com>
2022-09-16 13:56:18 -0300

    change structure

--- nvim/lua/user/autocommands.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd([[
-  augroup remember_folds
-    autocmd!
-    autocmd BufWinLeave * mkview
-    autocmd BufWinEnter * silent! loadview
-  augroup END
-]])
--- nvim/lua/user/bufferline.lua
+++
@@ -1,6 +0,0 @@
-require("bufferline").setup({
-	options = {
-		mode = "tabs",
-		diagnostics = "nvim_lsp",
-	},
-})
