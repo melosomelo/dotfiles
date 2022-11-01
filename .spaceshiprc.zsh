11bf97c009ed12f872572ba4bb2e7644269df088
melosomelo <mateusmelornrn@gmail.com>
2022-11-01 16:35:39 -0300

    synchronization commit 01/11/22 - 16:35:39

--- .spaceshiprc.zsh
+++ .spaceshiprc.zsh
@@ -1,6 +1,6 @@
 SPACESHIP_PROMPT_ADD_NEWLINE=false # don't add newline before each prompt
 
 SPACESHIP_USER_SHOW="always"
-SPACESHIP_USER_COLOR=cyan
+SPACESHIP_USER_COLOR=magenta
 
 SPACESHIP_DIR_SHOW=true
