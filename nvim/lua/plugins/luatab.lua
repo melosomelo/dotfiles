e99b6e0e202dc380e808cf632563b9aca33ef7de
melosomelo <mateusmelornrn@gmail.com>
2023-04-13 21:47:51 -0300

    synchronization commit 13/04/23 - 21:47:51

--- nvim/lua/plugins/luatab.lua
+++ nvim/lua/plugins/luatab.lua
@@ -40,11 +40,18 @@ local function title(bufnr)
 	elseif file == "" then
 		return "[No Name]"
 	else
+		-- Adds more components to the tab title (if required) to differentiate
+		-- it from other tabs' titles.
+		-- For example, if we had files src/routes/user.ts and src/services/user.ts,
+		-- instead of showing the entire path of the file or just user.ts on both
+		-- tabs, we'd have routes/user.ts and services/user.ts.
 		local tabs_names = get_other_tabs_names()
 		local bufname_path_components = util.split(vim.fn.bufname(bufnr), "/")
 		local left = #bufname_path_components
 		local final_title = bufname_path_components[left]
 		for i = 1, #tabs_names do
+			-- This will not enter an infinite loop with tabs that have the same file opened
+			-- as the funciton get_other_tabs_names ignores filters them.
 			while
 				util.join(util.slice(util.split(tabs_names[i], "/"), left), "/")
 				== final_title
