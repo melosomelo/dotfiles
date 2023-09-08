68a82f1f260ec586e8eea72a345be5a6acd54c03
melosomelo <mateusmelornrn@gmail.com>
2023-09-07 22:26:17 -0300

    fix file paths in script

--- setup.sh
+++ setup.sh
@@ -14,20 +14,20 @@ message "Generating fstab file"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 message "Setting the timezone"
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Fortaleza /etc/localtime
+ln -sf /usr/share/zoneinfo/America/Fortaleza /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
 
 message "Setting the locale"
-arch-chroot /mnt sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
-arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf
+sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /mnt/etc/locale.gen
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
 message "Setting the hostname"
 echo -n "Please, specify a hostname: "
 read hostname
-arch-chroot /mnt echo "${hostname}" > /etc/hostname
+echo "${hostname}" > /mnt/etc/hostname
 
 message "Setting the new root password"
-arch-chroot /mnt passwd
+passwd
 
 message "UEFI installing GRUB"
 arch-chroot /mnt pacman -S grub efibootmgr --noconfirm
