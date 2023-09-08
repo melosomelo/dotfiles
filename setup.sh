85a302f5f514fa50b52c75cbf818bd35325e94de
melosomelo <mateusmelornrn@gmail.com>
2023-09-07 21:50:12 -0300

    refine setup script

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ arch-chroot /mnt sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
 arch-chroot /mnt echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 message "Setting the hostname"
-echo "Please, specify a hostname:"
+echo -n "Please, specify a hostname: "
 read hostname
 arch-chroot /mnt echo "${hostname}" >> /etc/hostname
 
@@ -30,7 +30,7 @@ message "Setting the root password"
 arch-chroot /mnt passwd
 
 message "UEFI installing GRUB"
-arch-chroot /mnt pacman -S grub efibootmgr
+arch-chroot /mnt pacman -S grub efibootmgr --no-confirm
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
