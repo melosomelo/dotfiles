3147d5768eee61442f6dc7ebfdb8d03931664349
melosomelo <mateusmelornrn@gmail.com>
2023-09-13 11:43:38 -0300

    [2023-09-13_11-43] pacman install/remove hook

--- fish/functions/save_package_list.fish
+++ fish/functions/save_package_list.fish
@@ -1,5 +1,11 @@
 # doesn't deal with AUR packages yet
 function save_package_list
   cd $DOTFILES
-  pacman -Qqen | grep -v -E -e "base|linux|linux-firmware|grub|efibootmgr|networkmanager" - > wow.txt
+  # save any unstaged/uncomitted changes and pull any new changes
+  git add . && git stash && git pull && git stash pop
+  # Update the list of additional (not base system) packages
+  pacman -Qqen | grep -v -E -e "base|linux|linux-firmware|grub|efibootmgr|networkmanager" > packages.txt
+  git add .
+  git commit -m "[$(date +%Y-%m-%d_%H-%M)] pacman install/remove hook"
+  git push origin main
 end
