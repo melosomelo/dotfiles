531288f39c508c3682ffc9c7f2c04dce368bd050
melosomelo <mateusmelornrn@gmail.com>
2023-09-14 15:43:15 -0300

    fix setup script

--- setup.sh
+++ setup.sh
@@ -110,16 +110,16 @@ message "Downloading dotfiles"
 arch-chroot /mnt runuser -l "${username}" -c "git clone https://github.com/melosomelo/dotfiles && cd ${DOTFILES_DIR} && git submodule init && git submodule update"
 
 message "Setting up symbolic links"
-arch-chroot /mnt runuser -l "${username}" -c "mkdir .config"
+arch-chroot /mnt runuser -l "${username}" -c "mkdir -p .config"
 arch-chroot /mnt runuser -l "${username}" -c "ln -s ${DOTFILES_DIR}/X11/.xinitrc .xinitrc"
 arch-chroot /mnt runuser -l "${username}" -c "ln -s ${DOTFILES_DIR}/alacritty .config/alacritty"
-arch-chroot /mnt runuser -l "${username}" -c "mkdir .config/fish && ln -s ${DOTFILES_DIR}/fish/config.fish .config/fish/config.fish && ln -s ${DOTFILES_DIR}/fish/functions .config/fish/functions"
-arch-chroot /mnt runuser -l "${username}" -c "mkdir /etc/pacman.d/hooks && ln -s ${DOTFILES_DIR}/pacman/hooks/save_package_list.hook /etc/pacman.d/hooks/save_package_list.hook"
+arch-chroot /mnt runuser -l "${username}" -c "mkdir -p .config/fish && ln -s ${DOTFILES_DIR}/fish/config.fish .config/fish/config.fish && ln -s ${DOTFILES_DIR}/fish/functions .config/fish/functions"
+arch-chroot /mnt runuser -l "${username}" -c "sudo -S mkdir -p /etc/pacman.d/hooks && sudo ln -s ${DOTFILES_DIR}/pacman/hooks/save_package_list.hook /etc/pacman.d/hooks/save_package_list.hook"
 
 message "Installing Oh My Fish"
 arch-chroot /mnt runuser -l "${username}" -c 'curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > omf_install && fish omf_install --noninteractive --path=~/.local/opt/omf --config=~/.config/omf'
 
 message "Setting Oh My Fish theme"
-arch-chroot /mnt runuser-l "${username}" -c 'omf install l && omf theme l'
+arch-chroot /mnt runuser -l "${username}" -c 'omf install l && omf theme l'
 
 echo -e "${BOLD}Setup done! Reboot your machine!${RESET}"
