1335cad0fff343bcec745e772994a8f6d30aec17
melosomelo <mateusmelornrn@gmail.com>
2023-09-22 16:24:25 -0300

    add for aur packages in pacman hook

--- fish/functions/save_package_list.fish
+++ fish/functions/save_package_list.fish
@@ -4,7 +4,9 @@ function save_package_list
   # save any unstaged/uncomitted changes and pull any new changes
   git add . && git stash && git pull && git stash pop
   # Update the list of additional (not base system) packages
-  pacman -Qqen | grep -v -E -e "base|base-devel|linux|linux-firmware|grub|efibootmgr|networkmanager" > packages.txt
+  pacman -Qqen | grep -v -E -e "base|base-devel|linux|linux-firmware|grub|efibootmgr|networkmanager" > packages/official.txt
+  # AUR packages
+  pacman -Qqem > packages/aur.txt
   git add .
   git commit -m "[$(date +%Y-%m-%d_%H:%M)] pacman install/remove hook"
   git push origin main
