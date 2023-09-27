fcb5c01b588234e668df3abaa07b3348107fdfe1
melosomelo <mateusmelornrn@gmail.com>
2023-09-27 17:03:55 -0300

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ sudo sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5/g" /etc/pacman.conf
 
 username="mateus"
 message "Installing additional official packages"
-curl https://raw.githubusercontent.com/melosomelo/dotfiles/main/packages/official.txt > official.txt
+curl https://raw.githubusercontent.com/melosomelo/dotfiles/main/packages/official.txt > /home/$username/official.txt
 sudo pacman -S $(cat /home/$username/official.txt) --noconfirm && rm official.txt
 
 message "Setting new user's shell to fish"
