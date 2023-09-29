7abd8b1790d7ffc7a1bc78ac9fe148c65c4d65e9
melosomelo <mateusmelornrn@gmail.com>
2023-09-29 19:23:16 -0300

    wow

--- fish/functions/xev.fish
+++ fish/functions/xev.fish
@@ -1,8 +1,3 @@
 function xev
-<<<<<<< Updated upstream
   command xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'
 end
-=======
-  xev | awk -F'[
-end
->>>>>>> Stashed changes
