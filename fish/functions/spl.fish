3993f7f1f475000acd9c18293915740c013f3c2b
melosomelo <mateusmelornrn@gmail.com>
2024-06-14 14:09:44 -0300

    [2024-06-14_14:09] updating packages list

---
+++ fish/functions/spl.fish
@@ -0,0 +1,10 @@
+# Save Package List
+function spl
+  cd $DOTFILES
+  git stash clear && git add . && git stash && git pull && git stash pop
+  pacman -Qqen > packages/official.txt
+  pacman -Qqem > packages/aur.txt
+  git add .
+  git commit -m "[$(date +%Y-%m-%d_%H:%M)] updating packages list"
+  git push origin main
+end
--- fish/functions/ucel.fish
+++ fish/functions/ucel.fish
@@ -1,5 +1,9 @@
 # Update Code Extension List
 function ucel
+  cd $DOTFILES
+  git stash clear && git add . && git stash && git pull && git stash pop
   code --list-extensions > $HOME/dotfiles/code/extensions.txt
-  echo "Extension list was updated. Please, commit your changes."
+  git add .
+  git commit -m "[$(date +%Y-%m-%d_%H:%M)] updating vscode extensions list"
+  git push origin main
 end
