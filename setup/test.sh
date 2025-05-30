3717a22f6d8e44b572d0b90cf15c64121acade1e
melosomelo <mateusmelornrn@gmail.com>
2025-05-30 09:28:00 -0300

    [2025-05-30_09:28] updating packages list

--- setup/test.sh
+++ setup/test.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 set -e
 
 log() {
@@ -17,7 +19,30 @@ log() {
 
 arch_iso_download_url="$(curl --fail --silent --show-error 'https://archlinux.org/feeds/releases/' | xml sel -t -v '//channel/item[1]/link')torrent"
 
-log INFO "Downloading Arch Linux ISO from $arch_iso_download_url"
-aria2c --seed-time=0 --out=archlinux.iso $arch_iso_download_url
+if [ "$SKIP_ISO_DOWNLOAD" != 1 ]; then
+  log INFO "Downloading Arch Linux ISO from $arch_iso_download_url"
+  aria2c --seed-time=0 $arch_iso_download_url
+  mv $(ls | grep -E ".*\.iso\$") archlinux.iso
+fi
 
-log INFO "Creating virtual machine from ISO"
+if [ "$SKIP_ISO_REMASTER" != 1 ]; then
+  log INFO "Remastering ISO file with modified boot parameters"
+  mkdir -p ~/archiso/iso ~/archiso/work
+  mount -o loop archlinux.iso ~/archiso/iso
+  cp -rT ~/archiso/iso ~/archiso/work
+  umount ~/archiso/iso
+  sed -i 's/\(APPEND.*\)$/\1 console=ttyS0/' ~/archiso/work/boot/syslinux/archiso_sys-linux.cfg
+  rm -rf ~/archiso/iso ~/archiso/work
+fi
+
+# log INFO "Creating virtual machine from ISO"
+# virt-install \
+#   --name archlinux-test \
+#   --memory 2048 \
+#   --vcpus 2 \
+#   --cdrom archlinux.iso \
+#   --os-variant archlinux \
+#   --disk size=2 \
+#   --transient \
+#   --graphics none \
+#   --noautoconsole
