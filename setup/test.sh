c870c16190a6f275997fd743e04a15cf3a1d56f4
melosomelo <mateusmelornrn@gmail.com>
2025-05-30 10:28:50 -0300

    add autologin to created vm in test.sh

--- setup/test.sh
+++ setup/test.sh
@@ -25,19 +25,34 @@ if [ "$SKIP_ISO_DOWNLOAD" != 1 ]; then
   mv $(ls | grep -E ".*\.iso\$") archlinux.iso
 fi
 
+# We remaster the ISO file to add some behaviors that are required to properly run the scripts in a CI environment
 if [ "$SKIP_ISO_REMASTER" != 1 ]; then
   log INFO "Remastering ISO file"
+
+  # Mounting the ISO locally so we can edit it
   mkdir -p ~/archiso/iso ~/archiso/work
   mount -o loop archlinux.iso ~/archiso/iso
   cp -rT ~/archiso/iso ~/archiso/work
   umount ~/archiso/iso
 
+  # Send all output to the serial device ttyS0. Required to connect to the VM via virsh without any GUI
   sed -i 's/\(APPEND.*\)$/\1 console=ttyS0/' ~/archiso/work/boot/syslinux/archiso_sys-linux.cfg
 
-
+  # Enabling autologin on ttyS0
+  unsquashfs -d ~/archiso/airootfs ~/archiso/work/arch/x86_64/airootfs.sfs
+  mkdir -p ~/archiso/airootfs/etc/systemd/system/serial-getty@ttyS0.service.d
+  cat <<EOF > ~/archiso/airootfs/etc/systemd/system/serial-getty@ttyS0.service.d/autologin.conf
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear %I \$TERM
+EOF
+  rm ~/archiso/work/arch/x86_64/airootfs.sfs
+  mksquashfs ~/archiso/airootfs ~/archiso/work/arch/x86_64/airootfs.sfs -comp zstd -noappend
 
   script_dir=$(pwd)
   cd ~/archiso/work
+
+  # Rebuilding the ISO file
   xorriso -as mkisofs \
     -iso-level 3 \
     -full-iso9660-filenames \
