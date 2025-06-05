baf27b71239147f1c2caeac91393a66168b18b36
melosomelo <mateusmelornrn@gmail.com>
2025-06-05 20:31:20 -0300

    move and prune some old nvim config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,27 +1,36 @@
-return {{
-  "stevearc/conform.nvim",
-  event = "BufWritePre",
-  opts = require("configs.conform")
-}, -- These are some examples, uncomment them if you want to see them work!
-{
-  "neovim/nvim-lspconfig",
-  config = function()
-    require("configs.lspconfig")
-  end
-}, {
-  "nvim-tree/nvim-tree.lua",
-  opts = function()
-    return require("configs.nvimtree")
-  end
-} -- test new blink
--- { import = "nvchad.blink.lazyspec" },
--- {
--- 	"nvim-treesitter/nvim-treesitter",
--- 	opts = {
--- 		ensure_installed = {
--- 			"vim", "lua", "vimdoc",
---      "html", "css"
--- 		},
--- 	},
--- },
+return {
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		opts = require("configs.conform"),
+	}, -- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = function()
+			return require("configs.nvimtree")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"vimdoc",
+				"lua",
+				"luadoc",
+				"printf",
+				"javascript",
+				"typescript",
+				"c",
+				"cpp",
+			},
+			indent = { enable = true },
+		},
+	},
 }
