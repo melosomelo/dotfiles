7443ef9c4c430085cc6264b4ec0b8a6008cec301
melosomelo <mateusmelornrn@gmail.com>
2025-06-04 21:22:42 -0300

    [2025-06-04_21:22] updating packages list

--- env-bootstrap/setup.sh
+++ env-bootstrap/setup.sh
@@ -21,17 +21,18 @@ echo "Adding custom pacman configuration file"
 cp  dotfiles/pacman/pacman.conf /etc/pacman.conf
 
 echo "Adding pritunl repositories and keys"
-pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A | sudo pacman-key --add -
+sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 
 echo "Installing additional official packages"
 pacman -Sy --noconfirm $(cat dotfiles/packages/official.txt)
 
 echo "Installing yay"
-[-d yay] || runuser -u $USERNAME -- git clone https://aur.archlinux.org/yay.git
+[ -d yay ] || runuser -u $USERNAME -- git clone https://aur.archlinux.org/yay.git
 cd yay
 runuser -u $USERNAME -- makepkg -si
 cd..
 
 echo "Using yay to install AUR packages"
-yay -S $(cat dotfiles/packages/aur.txt)
+runuser -u $USERNAME -- yay -S $(cat dotfiles/packages/aur.txt)
