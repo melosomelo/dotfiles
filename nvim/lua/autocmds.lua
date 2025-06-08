31c07b8f9e93e36eb5bdf4ae2989c440cae3b029
melosomelo <mateusmelornrn@gmail.com>
2025-06-08 17:18:04 -0300

    add autocommand to ensure certain lsps are always installed

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,21 @@
-require "nvchad.autocmds"
+require("nvchad.autocmds")
+
+local autocmd = vim.api.nvim_create_autocmd
+
+local default_group = vim.api.nvim_create_augroup("default_group", { clear = true })
+autocmd("VimEnter", {
+	desc = "Ensure certain LSP servers are installed via Mason",
+	group = default_group,
+	callback = function()
+		local mason_registry = require("mason-registry")
+
+		local ensure_installed = { "clangd", "lua-language-server", "prettier", "stylua", "typescript-language-server" }
+
+		for _, server in ipairs(ensure_installed) do
+			if not mason_registry.is_installed(server) then
+				print("Server " .. server .. " is not installed. Installing it now...")
+				vim.cmd("MasonInstall " .. server)
+			end
+		end
+	end,
+})
